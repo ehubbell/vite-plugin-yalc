88ec4ddde789b37f7c7af08a352be8c5f444ef57
playbooks <eric@erichubbell.com>
2026-03-20 15:12:51 -0600

    param update

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,48 @@
 import { execSync } from 'node:child_process';
 
-export const runCommand = ({ command = '', format = '' }) => ({
-	apply: 'build',
-	name: 'run-command',
-	writeBundle: outputOptions => {
-		try {
-			if (format && format !== outputOptions.format) return;
-			execSync(command, { stdio: 'inherit' });
-			execSync('npx yalc push', { stdio: 'inherit' });
-		} catch (error) {
-			console.error(error);
-		}
-	},
-});
+export const runCommand = ({ command = '', format = '' }) => {
+	let outputOptions;
+	return {
+		apply: 'build',
+		name: 'run-command',
+		outputOptions(options) {
+			outputOptions = options;
+			return null;
+		},
+		closeBundle(error) {
+			try {
+				if (error) return;
+				if (format && format !== outputOptions.format) return;
+				execSync(command, { stdio: 'inherit' });
+				execSync('npx yalc push', { stdio: 'inherit' });
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	};
+};
 
-export const runYalc = ({ format = '' }) => ({
-	apply: 'build',
-	name: 'run-yalc',
-	writeBundle(outputOptions) {
-		try {
-			if (format && format !== outputOptions.format) return;
-			execSync('npx yalc push', { stdio: 'inherit' });
-		} catch (error) {
-			console.error(error);
-		}
-	},
-});
+export const runYalc = ({ format = '' }) => {
+	let outputOptions;
+
+	return {
+		apply: 'build',
+		name: 'run-yalc',
+		outputOptions(options) {
+			outputOptions = options;
+			return null;
+		},
+		closeBundle(error) {
+			try {
+				if (error) return;
+				if (format && format !== outputOptions.format) return;
+				execSync('npx yalc push', { stdio: 'inherit' });
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	};
+};
 
 // Docs
 // https://github.com/vitejs/vite/discussions/9217
